e19af9f770a1e15a34076a37b1c4fd69cca43656
milovanovic <barun@eecs.berkeley.edu>
2022-09-23 18:47:02 +0200

    Add new tests

--- src/test/scala/ZeroPadderSpec.scala
+++ src/test/scala/ZeroPadderSpec.scala
@@ -13,24 +13,40 @@ class ZeroPadderSpec extends AnyFlatSpec with Matchers {
 
   behavior of "Zero padding module"
 
-  it should f"work for UInt" in {
-    val params: ZeroPadderParams[UInt] = ZeroPadderParams(
-      proto = UInt(8.W),
-      packetSizeStart = 5,
-      packetSizeEnd = 8,
-      numberOfPackets = 3,
-      queueDepth = 8,
-      useBlockRam = false
-    )
-    Random.setSeed(11110L)
-    val in = Seq.fill(params.numberOfPackets)(Seq.fill(params.packetSizeStart)(Random.nextInt(1<<(params.proto.getWidth)).toDouble)) // max is exclusive 0 is inclusive
-    dsptools.Driver.execute(
-      () => new ZeroPadderNative(params),
-      Array(
-      "--backend-name", "verilator", // "treadle", // "verilator",
-      "--target-dir", s"test_run_dir/zeropadder")
-      ) { c =>
-        new ZeroPadderTester(c, in, 0)
-      } should be (true)
+  // useBlockRam needs to be always on true -> todo: make it work as well when useBlockRam is not true
+  // queue depth is set to be 2*packetSizeEnd + packetSizeEnd/2
+  //for (numberOfPackets <- Seq(3, 8, 5)) {
+  for (numberOfPackets <- Seq(5, 8)) {
+    for (packetSizeEnd <- Seq(16, 32)) {
+      val packetSizesStart = (2 to packetSizeEnd by 4).toSeq
+      for (packetSizeStart <- packetSizesStart) {
+        for (useQueue <- Seq(true, false)) {
+          for (useBlockRam <- Seq(true)) {
+        //for (useBlockRam <- Seq(true)) {
+          it should f"work for data type UInt(8.W), ct parameters numberOfPackets = $numberOfPackets, packetSizeEnd = $packetSizeEnd, useBlockRam = $useBlockRam, useQueue = $useQueue , rt parameter packetSizeStart = $packetSizeStart" in {
+            val params: ZeroPadderParams[UInt] = ZeroPadderParams(
+              proto = UInt(8.W),
+              packetSizeStart = packetSizeEnd,
+              packetSizeEnd = packetSizeEnd,
+              numberOfPackets = numberOfPackets,
+              queueDepth = packetSizeEnd*2 + packetSizeEnd/2,
+              useQueue = useQueue,
+              useBlockRam = true
+            )
+            Random.setSeed(11110L)
+            val in = Seq.fill(params.numberOfPackets)(Seq.fill(packetSizeStart)(Random.nextInt(1<<(params.proto.getWidth)).toDouble)) // max is exclusive 0 is inclusive
+            dsptools.Driver.execute(
+              () => new ZeroPadderNative(params),
+              Array(
+              "--backend-name", "verilator", // "treadle", // "verilator",
+              "--target-dir", s"test_run_dir/zeropadder")
+              ) { c =>
+                new ZeroPadderTester(c, in, 0, packetSizeStart)
+              } should be (true)
+            }
+          }
+        }
+      }
+    }
   }
 }
--- src/test/scala/ZeroPadderTester.scala
+++ src/test/scala/ZeroPadderTester.scala
@@ -5,14 +5,14 @@ import dsptools.DspTester
 import dsptools.numbers._
 import scala.util.Random
 
-class ZeroPadderTester[T <: Data](dut: ZeroPadderNative[T], in: Seq[Seq[Double]], tol: Int) extends DspTester(dut) {
+class ZeroPadderTester[T <: Data](dut: ZeroPadderNative[T], in: Seq[Seq[Double]], tol: Int, packetSizeStart: Int) extends DspTester(dut) {
   // here some kind of requirements should be added, check inp size and parameters of dut
   //require(dut.params.numberOfPackets == in.length)
   //require(in.foreach())
   //Random.setSeed(11110L)
 
   val inputData = in.flatten // this is total number of data -> if number of data is more than expected then
-  val numZeros = dut.params.packetSizeEnd - dut.params.packetSizeStart
+  val numZeros = dut.params.packetSizeEnd - packetSizeStart //dut.params.packetSizeStart
   var expectedData = in.map{c => c ++: Seq.fill(numZeros)(0.0)}.flatten
 
   expectedData.foreach { c => println(c.toString) }
@@ -21,7 +21,8 @@ class ZeroPadderTester[T <: Data](dut: ZeroPadderNative[T], in: Seq[Seq[Double]]
   var inValid = 0
   updatableDspVerbose.withValue(true) { // maybe true will become false
 
-    poke(dut.io.packetSizeStart, dut.params.packetSizeStart)
+    //poke(dut.io.packetSizeStart, dut.params.packetSizeStart)
+    poke(dut.io.packetSizeStart, packetSizeStart)
     poke(dut.io.packetSizeEnd, dut.params.packetSizeEnd)
     poke(dut.io.numberOfPackets, dut.params.numberOfPackets)
     step(5)
@@ -35,6 +36,9 @@ class ZeroPadderTester[T <: Data](dut: ZeroPadderNative[T], in: Seq[Seq[Double]]
             poke(dut.io.in.bits, input1.next())
           }
         }
+        else {
+          poke(dut.io.in.valid, 0)
+        }
       if (peek(dut.io.out.ready) == true && peek(dut.io.out.valid) == true) {
         dut.params.proto match {
           case dspR: DspReal => realTolDecPts.withValue(tol) { expect(dut.io.out.bits, expectedData.head) }
@@ -47,7 +51,6 @@ class ZeroPadderTester[T <: Data](dut: ZeroPadderNative[T], in: Seq[Seq[Double]]
         }
         expectedData = expectedData.tail
       }
-      // check last signal as well!
       step(1)
     }
   }
